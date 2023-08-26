9df2769f07477e51f57ec825b4785d7abe8ceca5
Ali3905 <muhammadali30905@gmail.com>
2023-08-26 11:01:55 +0500

    fixes

--- pages/components/Home/MockupSection.js
+++ pages/components/Home/MockupSection.js
@@ -4,56 +4,73 @@ import React from "react";
 const MockupSection = () => {
   return (
     <div className="mockup_section">
-      <div>
-        <Image src="/@1.jpg" width={100} height={500} />
-        <Image src="/@2.avif" width={100} height={500} />
-        <Image src="/@3.avif" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
+      <div className="left_to_mockup">
+        <div>
+          <div>
+            <Image src="/sq1.avif" width={100} height={500} />
+            <Image src="/sq2.avif" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt1.jpg" width={100} height={500} />
+            <Image src="/ld1.jpg" width={100} height={500} />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image src="/sq3.avif" width={100} height={500} />
+            <Image src="/sq4.avif" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt2.png" width={100} height={500} />
+            <Image src="/ld2.png" width={100} height={500} />
+          </div>
+        </div>
+        <div>
+          <div>
+          <Image src="/sq7.jpeg" width={100} height={500} />
+            <Image src="/sq8.png" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt7.png" width={100} height={500} />
+            <Image src="/ld3.jpg" width={100} height={500} />
+          </div>
+        </div>
       </div>
-      <div>
-        <Image src="/@4.avif" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-      </div>
-      <div>
-        <Image src="/@3.avif" width={100} height={500} />
-        <Image src="/@4.avif" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-      </div>
+
       <div className="main_mockup">
-        <video src='/mockup.mp4' autoPlay loop muted/>
+        <video src="/mockup.mp4" autoPlay loop muted />
       </div>
-      <div>
-        <Image src="/@7.jpeg" width={100} height={500} />
-        <Image src="/@1.jpg" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@2.avif" width={100} height={500} />
-      </div>
-      <div>
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@3.avif" width={100} height={500} />
-        <Image src="/@4.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-      </div>
-      <div>
-        <Image src="/@4.avif" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@5.avif" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
-        <Image src="/@6.jpg" width={100} height={500} />
+      <div className="left_to_mockup">
+        <div>
+          <div>
+            <Image src="/sq1.avif" width={100} height={500} />
+            <Image src="/sq2.avif" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt4.png" width={100} height={500} />
+            <Image src="/ld4.png" width={100} height={500} />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image src="/sq3.avif" width={100} height={500} />
+            <Image src="/sq4.avif" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt5.png" width={100} height={500} />
+            <Image src="/ld5.png" width={100} height={500} />
+          </div>
+        </div>
+        <div>
+          <div>
+            <Image src="/sq7.jpeg" width={100} height={500} />
+            <Image src="/sq8.png" width={100} height={500} />
+          </div>
+          <div>
+            <Image src="/pt6.png" width={100} height={500} />
+            <Image src="/ld6.jpg" width={100} height={500} />
+          </div>
+        </div>
       </div>
     </div>
   );
