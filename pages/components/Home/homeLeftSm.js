e805bdc2d324eba3f2a44b40ba6d970bfb7e9a0d
Ali3905 <muhammadali30905@gmail.com>
2023-09-14 18:51:49 +0500

    fixes

--- pages/components/Home/homeLeftSm.js
+++ pages/components/Home/homeLeftSm.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import NavbarSm from '../NavbarSm'
 import Menu from './Menu'
+import Script from 'next/script'
 
 const homeLeftSm = () => {
   const [showMenu, setShowMenu] = useState(false)
@@ -26,22 +27,25 @@ const homeLeftSm = () => {
   }
 
   useEffect(()=>{
-    console.log(showMenu);
-  },[showMenu])
+    // let circ = new CircleType(document.getElementById('circ'));
+    //     circ.raduis(360);
+  },[])
 
   return (
+    <>
+   
     <div className='bg'>
       <video src='/bg.mp4' autoPlay loop muted/>
     <div className='home_left_sm'>
      {!showMenu? <NavbarSm showMenu={showMenu} hideMenu={hideShowMenu} /> : null}
       
       <h2>
-        <span>We Create</span>
-        <span>Awesome</span>
+        <span>WE CREATE</span>
+        <span><i>A</i>WESOME</span>
         <span>
             {/* <ul> */}
             {/* <li> */}
-                Design
+                DESIGN
                 {/* </li> */}
             {/* <li>Branding</li>
             <li>Development</li> */}
@@ -49,7 +53,9 @@ const homeLeftSm = () => {
         </span>
       </h2>
       <div className='scroll'>
-        <p className='arrow'>&darr;</p>
+        <p className='arrow'>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+        </p>
         <div className='scroll_right'>
             <p>Scroll</p>
             <p></p>
@@ -63,11 +69,17 @@ const homeLeftSm = () => {
       {/* </button> */}
       {/* <div></div> */}
 
+     
+    
+
+    
+
       </div>
     </div>
 
     <Menu showMenu={showMenu} hideShowMenu={hideShowMenu}/>
     </div>
+    </>
   )
 }
 
